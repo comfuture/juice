f7f5802b4d1895d37f0a4a156b2a52b7297ed622
comfuture <comfuture@gmail.com>
2010-12-10 15:39:27 +0900

    experimental html5 element emulation

--- src/ui/core.js
+++ src/ui/core.js
@@ -1,1 +1,19 @@
-/* this will replaced with jquery-ui.min.js */
+juice.namespace('ui');
+juice.include('http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.6/jquery-ui.min.js');
+
+juice.ui.theme = function(theme) {
+	juice.css('http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.6/themes/' + theme  + '/jquery-ui.css');
+}
+juice.ui.theme(juice.options.t || 'base');
+juice(function() {
+	// html5 element hot fix
+	var types = {'date':'datepicker', 'datetime':'', 'range':'slider', 'number':''};
+	$.each(types, function(t, f) {
+		var el = document.createElement('input');
+		el.setAttribute('type', t);
+		var support = (el.type == t);
+		if (!support) {
+			try { $('input[type='+t+']')[f](); } catch(e) {}
+		}
+	});
+});
