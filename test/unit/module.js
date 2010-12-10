b6cfb018665987ad7293cece6bc8fe5a7b471e26
comfuture <comfuture@gmail.com>
2010-12-10 15:38:31 +0900

    changed juice.script.loaded -> juice.scripts, juice.module.loaded -> juice.modules. modified changes

--- test/unit/module.js
+++ test/unit/module.js
@@ -3,8 +3,8 @@ module('module');
 test('Module loader', function() {
 	expect(3);
 	ok(null != juice.ui.Flash, 'ui.Flash module loaded by ?m=ui.Flash option');
-	ok(juice.module.loaded.indexOf('ui.Flash') > -1, 'loaded module name is stored in juice.module.loaded');
-	ok(juice.script.loaded.indexOf('../src/ui/Flash.js') > -1, 'loaded script path is stored in juice.script.loaded');
+	ok(juice.modules.indexOf('ui.Flash') > -1, 'loaded module name is stored in juice.module.loaded');
+	ok(juice.scripts.indexOf('../src/ui/Flash.js') > -1, 'loaded script path is stored in juice.script.loaded');
 
 	var onAllLoaded = function() {
 		test('Module chain loader', function() {
